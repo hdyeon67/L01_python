ffa86e1bc3822db25d3ec8216877db316b3a1f56
educhel <node.chelsea@gmail.com>
2025-12-26 11:20:19 +0900

    [New] 파이썬 연습문제 배포

---
+++ python_practice/Q7.py
@@ -0,0 +1,15 @@
+# [실습 7]
+# 다음과 같은 api 데이터가 있을 때, 모든 유저들의 이름을 모아보시오.
+users = {
+    'total_user': 3,
+    'information': [
+        {'name': 'alex', 'age': 3, 'license': True},
+        {'name': 'june', 'age': 7, 'license': False},
+        {'name': 'peter', 'age': 4, 'license': False}
+    ]
+}
+
+names = []
+# 로직 작성
+
+print(names)  # ['alex', 'june', 'peter']
